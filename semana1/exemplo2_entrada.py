2067984e6a9bc636a8004429817eb8a4ee77b5fc
Maiara Freitas <maiara.freitas@alunos.ifsuldeminas.edu.br>
2024-02-19 10:13:44 -0300

    Aulas da semana 1

---
+++ semana1/exemplo2_entrada.py
@@ -0,0 +1,6 @@
+print()
+#entrada
+n1 = input("Digite o número 1:")
+n2 = input("Digite o número 2:")
+#saída
+print("você digitou", n1, n2)
---
+++ semana1/exemplo3_processamento.py
@@ -0,0 +1,13 @@
+print()
+#entrada
+n1 = input("Digite o número 1 :")
+n2 = input("Digite o número 2 :")
+
+#processamento
+soma = int(n1) + int(n2)
+
+#saída
+print("A soma é de", n1," + ", n2, "=", soma)
+# int ("10")-> converte o texto"10" para inteiro 10
+#float("1.5")-> converte o texto "1.5" para real 1.5
+#str(10)-> coverte o inteiro 10 para o texto"10"
